8a8e0dc0db588c6359f80e5bf59a40999512a462
root <root@vmi428573.contaboserver.net>
2020-10-31 08:14:23 +0100

    Doctype init

---
+++ iact/iact/doctype/corrective_action/corrective_action.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Manduul B. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Corrective Action', {
+	// refresh: function(frm) {
+
+	// }
+});
